1c6126d403d6e6f64051a59f5fc45b8ac96a17f4
heartrainy <526132448@qq.com>
2019-07-23 17:07:00 +0800

    添加用户

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -10,8 +10,19 @@ router.post('/getUserList', async (ctx, next) => {
   var pageSize = ctx.request.body.pageSize;
   const list = await User.find();
   const response = {}
+  response.code = 0;
   response.data = list;
   await delay(2000);
+  ctx.body = response;
+});
+
+router.post('/addUser', async (ctx, next) => {
+  const userFields = ctx.request.body;
+  await delay(2000);
+  await User.create(userFields);
+  const response = {}
+  response.code = 0;
+  response.data = {};
   ctx.body = response;
 });
 
